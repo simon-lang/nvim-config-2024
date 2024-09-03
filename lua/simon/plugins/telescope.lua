b7369e3bad3d033267fc9ad04d055e3bb4cfad39
Simon Lang <me@simonlang.org>
2024-09-03 13:01:23 +1000

    Telescope vertical layout

--- lua/simon/plugins/telescope.lua
+++ lua/simon/plugins/telescope.lua
@@ -1,5 +1,20 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.4",
-	dependencies = { "nvim-lua/plenary.nvim" },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.4",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local telescope = require("telescope")
+
+    telescope.setup({
+      defaults = {
+        layout_strategy = "vertical",
+        layout_config = {
+          vertical = {
+            width = 0.99,
+            height = 0.99
+          },
+        },
+      },
+    })
+  end,
 }
